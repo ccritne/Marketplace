c6c0084ceeee8d73ae6bdbaf52f8a88d2da5bf20
ccritne <info.cristiancinieri@gmail.com>
2025-01-30 11:44:52 +0100

    [update] Food class
    Add nutrional values property

--- src/Food.java
+++ src/Food.java
@@ -1,19 +1,41 @@
 public class Food extends Element {
 
     private Date expirationDate; 
+    private Nutritional values;
 
-    public Food(long id, String name, double price, long ean, String description, int stars, double weight, Date expirationDate) {
+    // Constructor
+    public Food(
+        long id, 
+        String name, 
+        double price, 
+        long ean, 
+        String description, 
+        int stars, 
+        double weight, 
+        Date expirationDate, 
+        Nutritional values
+    ) {
         super(id, name, price, ean, description, stars, weight);
         this.expirationDate = expirationDate;
+        this.values = values;
     }
 
     public Date getExpirationDate(){
         return expirationDate;
     }
 
+    public Nutritional getNutrionalValues(){
+        return values;
+    }
+
     public void setExpirationDate(Date expirationDate){
         this.expirationDate = expirationDate;
     }
+
+    public void setNutrionalValues(Nutritional values){
+        this.values = values;
+    }
+
 
     
 }
